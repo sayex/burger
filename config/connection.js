d09949e08f7bcf3453b6d7b9d03a1167a5a66cc3
sayex <elsayer@msn.com>
2019-02-12 16:06:05 -0700

    added schema and seeds data

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var database = require("env.js");
+var data = require("env.js");
 var connection;
 
 if (process.env.JAWSDB_URL){
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL){
 } else {
     connection = mysql.createConnection({
         host: "localhost",
-        user: "root",
-        password: "newpassword",
-        database: database
+        user: data.username,
+        password: data.password,
+        database: data.database
     });
 }
 
---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(45) NULL,
+    devoured BOOLEAN NULL,
+    PRIMIARY KEY (id)
+);
+
